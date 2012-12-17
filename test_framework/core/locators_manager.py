88981b5b7eddef8c7cbf906710689e800ab10a95
Nick Pettazzoni <npettazz@akamai.com>
2012-12-17 15:15:18 -0500

    better errors and documentation

--- test_framework/core/locators_manager.py
+++ test_framework/core/locators_manager.py
@@ -1,4 +1,5 @@
 from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException
 
 class LocatorsManager:
 
@@ -29,15 +30,21 @@ class LocatorsManager:
 
     def find_one(self, obj):
         results = self.find(obj)
+        if len(results) == 0:
+            raise Exception('Locator `%s` not found' % obj)
         return results[0]
 
     def find(self, obj):
         if type(obj) == str:
+            #a string definition key was passed in
             if obj in self.locators.keys():
+                #we have a definition for this locator key
                 return self.driver.find_elements(getattr(By, self.locators[obj]['type']), self.locators[obj]['locator'])
             else:
-                return self.find('CSS_SELECTOR', obj)
+                #default to By.CSS_SELECTOR if we don't recognize it
+                return self.find(('CSS_SELECTOR', obj))
         else:
+            #a tuple definition was passed in
             return self.driver.find_elements(getattr(By, obj[0]), obj[1])
 
     def get_locator(self, name):
