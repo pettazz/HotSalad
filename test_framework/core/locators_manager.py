0dd7d6b28b0e508981343a7b0fb1aeff8b98b7fa
Nick Pettazzoni <npettazz@akamai.com>
2013-02-06 15:00:11 -0500

    add documentation and allow add_locator to load Locator instances

--- test_framework/core/locators_manager.py
+++ test_framework/core/locators_manager.py
@@ -1,7 +1,11 @@
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException
 
+
 class Locator:
+    """
+    Represents an instance of a Locator for some webelement
+    """
     def __init__(self, webdriver, key_name, locator, by=By.CSS_SELECTOR):
         self.driver = webdriver
         self.key_name = key_name
@@ -12,9 +16,20 @@ class Locator:
         return "%s (%s)" % (self.key_name, self.locator)
 
     def find(self):
+        """
+        Find all instances that are identified by this locator
+        @returns
+        webelement or list of webelements
+        """
         return self.driver.find_elements(getattr(By, self.by), self.locator)
     
     def find_one(self):
+        """
+        Find exactly one instance identified by this locator. If multiple elements are found, returns the
+        first in the list.
+        @returns
+        webelement identified by this locator.
+        """
         results = self.driver.find_elements(getattr(By, self.by), self.locator)
         if len(results) == 0:
             raise NoSuchElementException('Locator `%s` (%s) not found.' % (self.key_name, self.locator))
@@ -22,7 +37,9 @@ class Locator:
 
 
 class LocatorsManager:
-
+    """
+    Helper class for creating and working with Locator instances
+    """
     def __init__(self, driver, locators_dict=None):
         self.driver = driver
         self.locators = {}
@@ -30,37 +47,87 @@ class LocatorsManager:
             self.add_locators(locators_dict)
 
     def __getattr__(self, name):
+        """
+        Allows us to access Locator instances stored within the helper directly as attributes
+        """
         if name in self.locators.keys():
             return self.locators[name]
         else:
             raise Exception('Locator `%s` not found' % name)
 
     def add_locators(self, locators_dict):
+        """
+        Add the given locators to the list of instances.
+        @params
+        locators_dict - Dictionary of Locators to be added where each key is the Locator name
+                        and each value is the corresponding Locator instance
+        """
         for x in locators_dict:
             self.add_locator(x, locators_dict[x])
 
     def add_locator(self, name, locator, locator_type=None):
+        """
+        Add a single Locator to the list of instances. If not already a Locator instance, 
+        it will be instantiated as one.
+        @params
+        name - name of the Locator
+        locator - one of: Locator instance to be added,
+                          tuple containing (value, locator type)
+                          String locator value
+        locator_type - String type of locator that is being added, as enumerated 
+                       in selenium.webdriver.common.by:By. one of:
+                            CSS_SELECTOR  (default)
+                            CLASS_NAME
+                            ID
+                            NAME
+                            LINK_TEXT
+                            XPATH
+                            TAG_NAME
+                            PARTIAL_LINK_TEXT
+        """
         if name in self.locators.keys():
             raise Exception('There is an existing locator with the name: %s' % name)
-        if locator_type is None:
-            if isinstance(locator, tuple):
-                locator_type = locator[0]
-                locator = locator[1]
-            else:
-                locator_type = 'CSS_SELECTOR'
+        if isinstance(locator, Locator):
+            self.locators[name] = locator
+        else:
+            if locator_type is None:
+                if isinstance(locator, tuple):
+                    locator_type = locator[0]
+                    locator = locator[1]
+                else:
+                    locator_type = 'CSS_SELECTOR'
 
-        if not hasattr(By, locator_type):
-            raise Exception('Unrecognized locator type: %s' % locator_type)
+            if not hasattr(By, locator_type):
+                raise Exception('Unrecognized locator type: %s' % locator_type)
 
-        self.locators[name] = Locator(self.driver, name, locator, locator_type)
+            self.locators[name] = Locator(self.driver, name, locator, locator_type)
 
     def find_one(self, locator):
+        """
+        Find exactly one instance identified by this locator. If multiple elements are found, returns the
+        first in the list.
+        @params
+        locator - one of: String Locator instance name
+                          String locator definition (using By.CSS_SELECTOR)
+                          tuple locator containing (value, locator type)
+        @returns
+        webelement identified by this locator.
+        """
         results = self.find(locator)
         if len(results) == 0:
             raise Exception('Locator `%s` not found' % locator)
         return results[0]
 
     def find(self, locator):
+        """
+        Find all instances that are identified by this locator
+        @params 
+        locator - one of: String Locator instance name
+                          String locator definition (using By.CSS_SELECTOR)
+                          tuple locator containing (value, locator type)
+        @returns
+        webelement or list of webelements
+        """
         if type(locator) == str:
             # a string definition key was passed in
             if locator in self.locators.keys():
@@ -75,12 +142,31 @@ class LocatorsManager:
             return self.driver.find_elements(getattr(By, obj[0]), obj[1])
 
     def find_child(self, webelement, locator):
+        """
+        Find exactly one instance that is a child of the given webelement, identified by this locator. 
+        If multiple elements are found, returns the first in the list.
+        @params
+        locator - one of: String Locator instance name
+                          String locator definition (using By.CSS_SELECTOR)
+                          tuple locator containing (value, locator type)
+        @returns
+        webelement identified by this locator.
+        """
         results = self.find_children(webelement, locator)
         if len(results) == 0:
             raise Exception('Locator `%s` not found' % locator)
         return results[0]
 
     def find_children(self, webelement, locator):
+        """
+        Find all instances that are children of a given webelement, identified by this locator.
+        @params 
+        locator - one of: String Locator instance name
+                          String locator definition (using By.CSS_SELECTOR)
+                          tuple locator containing (value, locator type)
+        @returns
+        webelement or list of webelements
+        """
         if type(locator) == str:
             # a string definition key was passed in
             if locator in self.locators.keys():
@@ -94,4 +180,11 @@ class LocatorsManager:
             return webelement.find_elements(getattr(By, obj[0]), obj[1])
 
     def get_locator(self, name):
+        """
+        Return the raw locator value of a given Locator name
+        @params
+        name - name of the Locator instance
+        @returns
+        String value of the locator, regardless of locator type.
+        """
         return self.locators[name].locator
